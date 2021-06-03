ef51d1f9272b4479092ff50dd9615b4e4ec2ea80
Harry Fuge <harry.fuge@gmail.com>
2021-06-03 14:07:08 +1000

    Lerping Successful
    And adjusting scripts

---
+++ ProjectMechlord/Assets/Scripts/GunManage.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunManage : MonoBehaviour
+{
+    // the manager to have the gun turrets set up to follow the player as well as rotate and lerp toward where the camera is looking
+
+
+    public Transform cameraTransform;
+    Transform[] gunTransforms;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //because this is set up in the parent gameobject,
+        //get the transforms of the children
+        gunTransforms = GetComponentsInChildren<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //each frame set the position of the parent GO to be set to the body (currently set to the main camera)
+        transform.position = cameraTransform.transform.position;
+
+        //set the rotation of the children to lerp towards where the camera looks
+        for (int i = 0; i < gunTransforms.Length; i++)
+        {
+            gunTransforms[i].rotation = Quaternion.Lerp(gunTransforms[i].rotation, cameraTransform.transform.rotation, 0.01f);
+        }
+
+
+    }
+}
---
+++ ProjectMechlord/Assets/Scripts/MovementController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+
+    public CharacterController controller;
+
+    public int speed = 10;
+
+   
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 direction = transform.right * x + transform.forward * z;
+
+        controller.Move(direction * speed * Time.deltaTime);
+        
+    }
+}
