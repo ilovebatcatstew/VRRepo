810bb8e6694b8bec128ad1730a51b53e263a3219
Harry Fuge <harry.fuge@gmail.com>
2021-06-10 09:18:38 +1000

    beginnings of SO work

---
+++ ProjectMechlord/Assets/Scripts/EnemyCreation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[CreateAssetMenu(fileName = "Enemy", menuName = "Scripting/Enemy", order = 1)]
+public class EnemyCreation : ScriptableObject
+{
+    private GameObject gameObject;
+    public float currentHP;
+    public float maxHP;
+    public NavMeshAgent enemyNav;
+
+    public void Awake()
+    {
+        
+    }
+
+
+
+}
