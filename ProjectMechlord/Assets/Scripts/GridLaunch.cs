29f22b9894a4b0f0c8c9b5474a58cea2284b8214
Vlad <s200303@aie.edu.au>
2021-06-17 09:28:43 +1000

    "Cool" Missile launch script complete.
    Oh also Unity added some stupid SteamVR settings.

--- ProjectMechlord/Assets/Scripts/GridLaunch.cs
+++ ProjectMechlord/Assets/Scripts/GridLaunch.cs
@@ -44,6 +44,8 @@ public class GridLaunch : MonoBehaviour
 
         objectSpawnPosition = gameObject.transform;
 
+        StartCoroutine(DebugLaunchVolley());
+
         //StartCoroutine(LaunchVolley());
         //Instantiate(launchObject, transform.position, transform.rotation);
     }
@@ -55,7 +57,7 @@ public class GridLaunch : MonoBehaviour
 
     private void FixedUpdate()
     {
-        DebugLaunchVolley();
+        //DebugLaunchVolley();
         //timer += Time.deltaTime;
         //if (mouse.rightButton.isPressed && timer > cooldown)
         //{
@@ -64,49 +66,36 @@ public class GridLaunch : MonoBehaviour
         //}
     }
 
-    void  DebugLaunchVolley()
+    IEnumerator DebugLaunchVolley()
     {
-        // Start point should be on the 'left.
+        Vector3 right = transform.right;
+        Vector3 up = transform.up;
+        Vector3 forward = transform.forward;
 
-        Vector3 xStart = -transform.right * width * 0.5f; // This starts it off at the left edge of the object this is attached to.
-        Vector3 xEnd = transform.right * width * 0.5f; // This is the end point of where the projectiles should spawn
 
-        Vector3 yStart = transform.up * height * 0.5f;
-        //Vector3 yStart = 
+        float xOffset = width / columns;
+        float yOffset = height / rows;
 
-        Vector3 zStart = transform.forward * length * 0.5f;
+        float smallerWidth = width - 2 * xOffset;
+        float smallerHeight = height - 2 * yOffset;
 
-        //xStart.x += launchObject.transform.localScale.x * 0.5f; // Offset a little bit from the edge based on the projectile's width
-        xStart.x += width / columns; // offset by a bit from the edge of the object
-        xEnd.x -= width / columns;
+        float xStartPos = smallerWidth * 0.5f;
+        float yStartPos = smallerHeight * 0.5f;
 
-        yStart.y -= height / rows;
+        float columnOffset = smallerWidth / (columns - 1);
+        float rowOffset = smallerHeight / (rows - 1);
 
-        Vector3 xOffset = new Vector3(1, 0, 0) * (width / columns);
-        Vector3 yOffset = new Vector3(0, 1, 0) * (height / rows);
-
-       
-       // Debug.DrawLine(objectSpawnPosition.position, objectSpawnPosition.position + objectSpawnPosition.forward * 3, Color.white);
-
-        for(int x = 0; x < columns - 1; x++)
+        for (int x = 0; x < columns; x++)
         {
-            for(int y = 0; y > -rows + 1; y--)
+            for (int y = 0; y < rows; y++)
             {
-            Debug.DrawLine(gameObject.transform.position + xStart + (xOffset * x) + yStart + (yOffset * y) + zStart, gameObject.transform.position + xStart + (xOffset * x) + yStart + (yOffset * y) + zStart + gameObject.transform.forward * 3, Color.white);
-        //        //    Instantiate(launchObject, gameObject.transform.position + xStart + (xOffset * x) + yStart + (yOffset * y) + zStart, gameObject.transform.rotation);
-        //        //yield return new WaitForSeconds(launchInterval);
+                Debug.DrawLine(transform.position + right * (-xStartPos + (x * columnOffset)) + up *(yStartPos - (y * rowOffset)), transform.position + right * (-xStartPos + (x * columnOffset)) + up * (yStartPos - (y * rowOffset)) + forward, Color.white);
+                Instantiate(launchObject, transform.position + right * (-xStartPos + (x * columnOffset)) + up * (yStartPos - (y * rowOffset)), gameObject.transform.rotation);
+                yield return new WaitForSeconds(launchInterval);
             }
-        //    //Instantiate(launchObject, gameObject.transform.position, gameObject.transform.rotation);
         }
 
 
-        //Instantiate(launchObject, gameObject.transform.position + xStart, gameObject.transform.rotation);
-
-        //for(int x = 0; x < columns; x++)
-        //{
-        //    Vector3 offset = new Vector3(transform.right.x * (x - columns * 0.5f * launchObjectWidth), transform.position.y, transform.position.z);
-        //    Instantiate(launchObject, transform.position + offset * (width * 0.1f), transform.rotation);
-        //}
     }
 
     IEnumerator LaunchVolley()
