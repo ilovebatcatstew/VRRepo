24cb087ab570658baaa3ff0bbc3c7a211fdd11cb
Harry <harry.fuge@gmail.com>
2021-06-17 10:49:33 +1000

    EnemySO
    Creating Scriptable objects for the enemies

---
+++ ProjectMechlord/Assets/Scripts/EnemySOStatCreate.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemyStats", menuName ="CreateEnemyStats", order = 1)]
+public class EnemySOStatCreate : ScriptableObject
+{
+    public string enemyType;
+    public float maxHealth;
+    public float currenthealth;
+    public float movespeed;
+    public float damage;
+
+}
