0dbab6266b58cb1e664570c25da1478213f967d7
Harry <harry.fuge@gmail.com>
2021-06-18 11:03:24 +1000

    Updated Enemy Mechs with them firing back
    also updated the prefabs to have the enemymechv2 as its own prefab

---
+++ ProjectMechlord/Assets/Resources/Scripts/EnemyProjectile.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyProjectile : MonoBehaviour
+{
+    public float range = 10.0f;
+    public float projectileSpeed = 1.0f;
+    public int damage = 1;
+
+    float distanceTravelled = 0.0f;
+
+    Collider collider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        collider = gameObject.GetComponent<SphereCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(distanceTravelled >= range)
+        {
+            Destroy(gameObject);
+        }        
+    }
+
+    private void FixedUpdate()
+    {
+        distanceTravelled += projectileSpeed * Time.deltaTime;
+        transform.position += transform.forward * projectileSpeed;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            // Some kind of take damage function or health variable that gets subtracted from
+            Destroy(gameObject);
+        }
+
+        if (!other.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
